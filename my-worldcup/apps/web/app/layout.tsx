265ab469e2aaa0dada197ac02752122e5ef5aea8
hyunbell <man.of.backend@gmail.com>
2023-03-23 23:20:48 +0900

    :rocket: tailwind 추가 작업

--- my-worldcup/apps/web/app/layout.tsx
+++ my-worldcup/apps/web/app/layout.tsx
@@ -1,3 +1,10 @@
+import './global.css';
+
+export const metadata = {
+  title: '이상형 월드컵 게임',
+  description: 'hyunBell',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- my-worldcup/apps/web/app/page.tsx
+++ my-worldcup/apps/web/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>테스트 월드컵 게임</h1>;
+  return <h1 className="text-3xl font-bold underline">테스트 월드컵 게임</h1>;
 }
