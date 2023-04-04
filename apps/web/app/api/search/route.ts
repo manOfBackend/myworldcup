71590304e4b1d3463e4ac6fb88fe4b6c40bf4e56
hyunbell <man.of.backend@gmail.com>
2023-04-04 23:19:51 +0900

    chore: api routes test

--- apps/web/app/api/search/route.ts
+++ apps/web/app/api/search/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 
 import list from "../../../mock/games.json";
-
+import { postSuggest } from 'lib/src/api'
 export async function GET(req: Request) {
+  postSuggest('keyword')
   const { searchParams } = new URL(req.url);
   const name = searchParams.get("name");
   return NextResponse.json(list);
