541caf7a498765a931b350a8fdc11a576e00f9f3
hyunbell <man.of.backend@gmail.com>
2023-04-05 23:00:52 +0900

    chore: login page test sample

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,14 +1,20 @@
+import Script from 'next/script';
 import './global.css';
 
 export const metadata = {
-  title: '이상형 월드컵 게임',
+  title: 'hyunBell 토이 프로젝트',
   description: 'hyunBell',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>HY</title>
+      </head>
       <body>{children}</body>
+      <Script src='https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js'/>
+      <Script src='https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js' noModule/>
     </html>
   );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,25 @@
 import { PromptInput } from "ui";
 
 export default function Page() {
-  return <main>
-    <h1 className="text-3xl font-bold underline">ChatGPT 이미지 만들기</h1>
-    <PromptInput />
-  </main> 
-    
+  return (
+    <section>
+      <div className="form-box">
+        <div className="form-value">
+          <form action="">
+            <h2>로그인</h2>
+            <div className="inputbox">
+              <input id='email' type="email" required />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="inputbox">
+              <input id='password' type="password" required />
+              <label htmlFor="password">Password</label>
+            </div>
+            <button>Log in</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+
 }
