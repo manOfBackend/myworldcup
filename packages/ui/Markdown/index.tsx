a2a7e72a032d548098183f371f9af15fac0b3f54
hyunbell <man.of.backend@gmail.com>
2023-04-08 23:41:24 +0900

    feat: markdown ui

---
+++ packages/ui/Markdown/index.tsx
@@ -0,0 +1,60 @@
+import { copyToClipboard } from "@hgpt/lib/clipboard";
+import { RefObject, useEffect, useRef, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import RemarkBreaks from "remark-breaks";
+import RemarkGfm from "remark-gfm";
+import RemarkMath from "remark-math";
+
+export function PreCode(props: { children: any }) {
+  const ref = useRef<HTMLPreElement>(null);
+
+  return (
+    <pre ref={ref}>
+      <span
+        className="copy-code-button"
+        onClick={() => {
+          if (ref.current) {
+            const code = ref.current.innerText;
+            copyToClipboard(code);
+          }
+        }}></span>
+      {props.children}
+    </pre>
+  );
+}
+
+const useLazyLoad = (ref: RefObject<Element>): boolean => {
+  const [isIntersecting, setIntersecting] = useState<boolean>(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setIntersecting(true);
+        observer.disconnect();
+      }
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+};
+
+export default function Markdown(props: { content: string }) {
+  return (
+    <ReactMarkdown
+      remarkPlugins={[RemarkMath, RemarkGfm, RemarkBreaks]}
+      components={{
+        pre: PreCode,
+      }}
+      linkTarget={"_blank"}>
+      {props.content}
+    </ReactMarkdown>
+  );
+}
