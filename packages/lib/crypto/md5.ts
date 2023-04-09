5b12e804ac10ada4976ec999031c3ec4ea5a685e
hyunbell <man.of.backend@gmail.com>
2023-04-10 00:53:46 +0900

    feat: crypto lib

---
+++ packages/lib/crypto/md5.ts
@@ -0,0 +1,1 @@
+export { default as md5 } from "spark-md5";
--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -3,3 +3,5 @@ export { default as clipboard } from "./clipboard";
 export { type Chat, type Message } from "./types";
 export { default as sessionController } from "./sessionController";
 export * as request from "./request";
+export * as crypto from "./crypto";
+export * from "./eventsource-parser";
