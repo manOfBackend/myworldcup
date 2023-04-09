e9760ffc732d4c65a890b3849436bb310e485d1f
hyunbell <man.of.backend@gmail.com>
2023-04-10 00:55:16 +0900

    fix: openai env key name

--- apps/web/openai.ts
+++ apps/web/openai.ts
@@ -3,8 +3,8 @@ import { OpenAIApi } from "openai";
 import { Configuration } from "openai";
 
 const config = new Configuration({
-  organization: process.env.OPEN_AI_ORGANIZATION_ID,
-  apiKey: process.env.OPEN_AI_KEY,
+  organization: process.env.OPENAI_ORGANIZATION_ID,
+  apiKey: process.env.OPENAI_KEY,
 });
 
 const openai = new OpenAIApi(config);
