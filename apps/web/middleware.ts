8d9b52c5cbfa5879408e6ded59e9391b65d812b9
hyunbell <man.of.backend@gmail.com>
2023-04-24 02:35:19 +0900

    fix: chat api-key middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 import { getDecodedToken } from "./auth/util";
 
 export const config = {
-  matcher: ["/", "/chat", "/((?!_next/static|favicon.ico|logo.svg).*)"],
+  matcher: ["/", "/chat", "/((?!_next/static|favicon.ico|logo.svg).*)", "/api/chat-stream"],
 };
 
 export async function middleware(req: NextRequest) {
@@ -32,6 +32,24 @@ export async function middleware(req: NextRequest) {
   if (url.pathname === "/signin" && decodedToken) {
     url.pathname = "/chat";
     return NextResponse.redirect(url);
+  }
+
+  if (url.pathname === "/api/chat-stream" && decodedToken) {
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
+    const apiKey = process.env.OPENAI_KEY;
+    if (apiKey) {
+      req.headers.set("token", apiKey);
+    } else {
+      return NextResponse.json(
+        {
+          error: true,
+          msg: "Empty Api Key",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
   }
 
   return NextResponse.next({
